03e8497e5b64eed90a94fa08f2a70f0c31d01655
rulyanaarriol-utp <rarriolamamanillo82@gmail.com>
2026-07-03 22:34:39 -0400

    mis cambios del mapa

--- src/components/institucionalidad/situacion-geografica.jsx
+++ src/components/institucionalidad/situacion-geografica.jsx
@@ -10,7 +10,7 @@ function SituacionGeografica() {
       </p>
       <iframe 
   className="mapa"
-  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3717.1234!2d-65.756789!3d-19.588234!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x93e2b7b8a9b8c7d6%3A0x1234567890abcdef!2sSeguro%20Social%20Universitario%20de%20Potos%C3%AD%2C%20C.%20Calama%20107%2C%20Potos%C3%AD!5e0!3m2!1ses!2sbo!4v1234567890"
+  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3695.2!2d-65.7568!3d-19.5882!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x93e2b7b8a9b8c7d6%3A0x1234567890abcdef!2sSeguro%20Social%20Universitario%20de%20Potos%C3%AD%2C%20C.%20Calama%20107%2C%20Potos%C3%AD!5e0!3m2!1ses!2sbo!4v1700000"
   width="100%" 
   height="450" 
   style={{border:0}} 
